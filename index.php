a2ecd3e3c91fb8eb48991e1710cb8d00dde83710
AndrewLaird <alaird@uci.edu>
2017-04-10 18:53:30 -0700

    First Push

---
+++ index.php
@@ -0,0 +1,21 @@
+<header>
+</header>
+
+<body>
+<h1>Andrew Laird, best Coder</h1>
+
+<p>Welcome to my site! its really great isn't it</p>
+
+</body>
+
+<style>
+h1{
+	color:blue;
+	margin:auto;
+
+}
+p{
+	margin:auto;
+}
+
+</style>
