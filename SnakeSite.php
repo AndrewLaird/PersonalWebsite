8c8e18d2c91bc3a3ae7b81aaf55fdb7c9620de8c
AndrewLaird <alaird@uci.edu>
2017-08-21 07:48:43 -0700

    fixing snake

--- SnakeSite.php
+++ SnakeSite.php
@@ -104,12 +104,12 @@ function SnakeArt(id) {
       var x = snakes[i][0];
       var y = snakes[i][1];
 
-      //if(board[y][x] == 1){
-      colorDict = {0:color1,1:color2,2:color3,3:color4};
-      c.fillStyle =colorDict[i]
-      c.fillRect(x * xBox, y * yBox, xBox, yBox );
-      //board[y][x] = 1;
-      //}
+      if(board[y][x] == 1){
+        colorDict = {0:color1,1:color2,2:color3,3:color4};
+        c.fillStyle =colorDict[i]
+        c.fillRect(x * xBox, y * yBox, xBox, yBox );
+        board[y][x] = 1;
+      }
     }
     //does this every 50 miliseconds
   }, .001);
