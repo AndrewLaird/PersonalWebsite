c043d4421ec1d7f61dd8f8278b4b203f5f25f8e1
AndrewLaird <alaird@uci.edu>
2020-06-12 17:24:09 -0700

    spelling and font color

--- projects/index.php
+++ projects/index.php
@@ -28,7 +28,7 @@
      <div class="caption">
         <p>PPO
         </br>Github repo: https://github.com/AndrewLaird/AI_Club_PPO </br>
-        </br>Similar to AlphaZero, I wanted to look into the Proximal Policy Optimization (PPO) reinforcement learning algorithm. What is significant about PPO is how fast it learns to play games. I trained the AI below on a laptop computer in about an hour. Alot of the popular reinforcement learning algorithms before this focused on predicting how good -- based on recieved reward-- to be in a specific state of the environment. PPO and othe rPolicy Optimization algorithms focus just on predicting a policy that maximizes the recieved reward. Because of this they are less robust and explainable but solve certain problems much better. 
+        </br>Similar to AlphaZero, I wanted to look into the Proximal Policy Optimization (PPO) reinforcement learning algorithm. What is significant about PPO is how fast it learns to play games. I trained the AI below on a laptop computer in about an hour. Alot of the popular reinforcement learning algorithms before this focused on predicting how good -- based on received reward-- to be in a specific state of the environment. PPO and other Policy Optimization algorithms focus just on predicting a policy that maximizes the received reward. Because of this they are less robust and explainable but solve certain problems much better. 
         </p>
       </div>
       <img src="./BipedalWalker2.gif"  alt="BipdealWalker" style="width:100%">
@@ -42,8 +42,8 @@
 
     <div class="polaroid">
      <div class="caption">
-        <p>Google Irvine's Tech Challege
-        </br>This was a fanastic oportuniy, Google invited teams of college students to compete over tiny challenges, some programming, some logic puzzles, some that required pop culture knowledge. It was a day of fun and solving problems that you would really only hear about in books. Out team solved the most challenges and won the competition but the real reward was just being in that room and having fun being computer science students  </p>
+        <p>Google Irvine's Tech Challenge
+        </br>This was a fantastic opportuniy, Google invited teams of college students to compete over tiny challenges, some programming, some logic puzzles, some that required pop culture knowledge. It was a day of solving problems and working as a team to use each persons strengths. Our team solved the most challenges and won the competition but the real reward was just being in that room and having fun being computer science students  </p>
       </div>
       <img src="./GoogleIrvine.jpeg"  alt="Google Irvine image" style="width:100%">
       
@@ -52,7 +52,7 @@
     <div class="polaroid">
      <div class="caption">
         <p>Ragan Lab:
-        </br>I left INRF because I wanted to learn more about machine learning. I found a grad student that was working on his phd in Material Science. William Thrift, was an amazing mentor and introduction to machine learning. We had a goal of what we wanted for the paper he was working on, but we were both learning how to achieve that goal and I learned alot of the basics from talking back and forth with him. The paper that I and a couple of other undergraduates helped him write ended being published in ACS sensors. Which makes me a published author, this was a fun benifit but what I really got from working in the Ragan Lab was tons of knowledge about machine learning that would help me learn more on my own. Below is the image from the abstract of that paper</p>
+        </br>I left INRF because I wanted to learn more about machine learning. I found a grad student that was working on his PhD in Material Science. William Thrift was an amazing mentor and introduction to machine learning. We had a goal of what we wanted for the paper he was working on, but we were both learning how to achieve that goal and I learned a lot of the basics from talking back and forth with him. The paper that I and a couple of other undergraduates helped him write ended being published in ACS Sensors. Which makes me a published author, this was a fun benefit but what I got from working in the Ragan Lab was tons of knowledge about machine learning that would help me learn more on my own. Below is the image from the abstract of that paper</p>
       </div>
       <img src="./RaganACS.gif"  alt="Picture from acs paper"  style="width:100%">
       
@@ -61,7 +61,7 @@
     <div class="polaroid">
      <div class="caption">
         <p>INRF:
-        </br>My first year at UCI I was incredibly fortunate to find a lab that needed fullstack work done and had the pacients to teach me. I worked with one software developer and a lab full of engineers to provide meaningful information. Technicians would create circuits in this lab and the process of doing that is very inconsistent. To help them understand why their experimetns were failing, I built a sytem that measured the climate in the lab at any particular moment. So they could look back and see if the lab spiked out of reasonable conditions during their experiment. One of the best parts of this project was the lack of direction, we had a final goal but other than that, I was given a budget and blank desktop computer. So I installed linux on the computer and went to work making a network of raspbery pi's to measure the climate and report it to the main server. From there I made the API public and a more senior engineer took that data and integrated it into the interface you see below. The second picture is the raspberry pi's before they were installed</p>
+        </br>During my first year at UCI I was incredibly fortunate to find a lab that needed fullstack work done and had the time to let me learn. I worked with one software developer and a lab full of engineers to provide meaningful information. Technicians would create circuits in this lab and the process of doing that is very inconsistent. To help them understand why their experiments were failing, I built a system that measured the climate in the lab at any particular moment. So they could look back and see if the lab spiked out of reasonable conditions during their experiment. One of the best parts of this project was the lack of direction, we had a final goal but other than that, I was given a budget and blank desktop computer. So I installed linux on the computer and went to work making a network of raspberry pi's to measure the climate and report it to the main server. From there I made the API public and a more senior engineer took that data and integrated it into the interface you see below. The second picture is the raspberry pi's before they were installed</p>
       </div>
       <img src="./inrf_temp_humid.gif"  alt="gif showing inrf system" style="width:100%">
       <img src="./raspberryPi.jpg"  alt="gif showing inrf system" style="width:100%">
