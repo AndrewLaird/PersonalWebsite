525de8d356491f1a96f283e4d0477d5ecda98fe6
AndrewLaird <lairdandrew11@gmail.com>
2021-05-18 17:26:31 -0700

    updating text

--- projects/index.php
+++ projects/index.php
@@ -12,9 +12,39 @@
 
     <div class="polaroid">
      <div class="caption">
-        <p>AlphaZero TicTacToe
-            </br>Github repo: https://github.com/AndrewLaird/alpha_tictactoe</br>
-            </br>Applied Deepmind's AlphaZero Reinforcement Learning algorithm to learn tictactoe from scratch. This is overkill but I wanted to dive into the AlphaZero algoritm without requiring a huge amount of computational power
+        <p class='bolder'>Chess Detector </p>
+	    <a href="http://chessdetector.com">ChessDetector.com </a>
+	</p>
+	<p class="left">
+            Takes images of chess boards and ouptuts the chessboards in a format that the major chess websites understand. The goal of this project was to add something to the wonderful chess community. I got into chess when queens gambit came out like everyone else. I was surprised by how much video content there was to help anyone become a better player. This tool allows those videos to be even more helpful. A learner can take a position from a video they are watching and deep dive into it using one of the chess analyzers such as lichess or Chess.com. This project was fun for me because I was able to use my ai skills in a real world context and learn React while I was at it.
+        </p>
+      </div>
+        <div class="caption">
+          <h5>Here's the model running on a dificult image with lots of arrows obstructing the pieces</h5>
+        </div>
+      <img src="./ChessDetector.png"  alt="ChessDetector" style="width:100%">
+    </div>
+
+    <div class="polaroid">
+     <div class="caption">
+        <p><p class='bolder'>Keyboard</p>
+            </br>
+	      <img src="./GlamorKeyboard.jpg"  alt="Keyboard" style="width:50%" align='left'>
+	</p>
+	<p class="left">
+I built a handwired split ergonomic keyboard. Keyboards like this have been popping up in the last 5 years after a talk by Matt Adereth <a href="https://www.youtube.com/watch?v=uk3A41U0iO4&ab_channel=ClojureTV">(link)</a>.  Below is what the underside looks like. It was a mess of soldering at all sorts of weird angles but after a couple sessions, I got the whole keyboard working. I respect that this keyboard is a silly but its really fun to do your work on something that you built yourself. And the added benifit of being able to use QMK firmware and all of it's functionality makes me never want to go back.
+	</p>
+      </div>
+      <img src="./HandWiredKeyboard.jpg"  alt="HandWiredKeyboard" style="transform:rotate(270de); width:100%; ">
+    </div>
+
+    <div class="polaroid">
+     <div class="caption">
+        <p><p class='bolder'>AlphaZero TicTacToe</p>
+            <a href="https://github.com/AndrewLaird/alpha_tictactoe">(Github repo)</a>
+	</p>
+	<p class="left">
+            Applied Deepmind's AlphaZero Reinforcement Learning algorithm to learn tictactoe from scratch. This is overkill but I wanted to dive into the AlphaZero algoritm without requiring a huge amount of computational power
         </p>
       </div>
         <div class="caption">
@@ -26,9 +56,11 @@
 
     <div class="polaroid">
      <div class="caption">
-        <p>PPO
-        </br>Github repo: https://github.com/AndrewLaird/AI_Club_PPO </br>
-        </br>Similar to AlphaZero, I wanted to look into the Proximal Policy Optimization (PPO) reinforcement learning algorithm. What is significant about PPO is how fast it learns to play games. I trained the AI below on a laptop computer in about an hour. Alot of the popular reinforcement learning algorithms before this focused on predicting how good -- based on received reward-- to be in a specific state of the environment. PPO and other Policy Optimization algorithms focus just on predicting a policy that maximizes the received reward. Because of this they are less robust and explainable but solve certain problems much better. 
+        <p><p class='bolder'>PPO</p>
+        <a href="https://github.com/AndrewLaird/AI_Club_PPO">(Github repo)</a>
+	</p>
+	<p class="left">
+        I wanted to look into the Proximal Policy Optimization (PPO) reinforcement learning algorithm. What is significant about PPO is how inexpensive it is to train in comparision to the previous algorithms. I trained the AI below on a laptop computer in about an hour. A lot of the popular reinforcement learning algorithms before this focused on predicting how good -- based on received reward-- to be in a specific state of the environment. PPO and other Policy Optimization algorithms focus just on predicting a policy that maximizes the received reward. Because of this they are less robust and explainable but solve certain problems much better. 
         </p>
       </div>
       <img src="./BipedalWalker2.gif"  alt="BipdealWalker" style="width:100%">
@@ -42,8 +74,11 @@
 
     <div class="polaroid">
      <div class="caption">
-        <p>Google Irvine's Tech Challenge
-        </br>This was a fantastic opportuniy, Google invited teams of college students to compete over tiny challenges, some programming, some logic puzzles, some that required pop culture knowledge. It was a day of solving problems and working as a team to use each persons strengths. Our team solved the most challenges and won the competition but the real reward was just being in that room and having fun being computer science students  </p>
+        <p><p class='bolder'>Google Irvine's Tech Challenge</p>
+	</p>
+	<p class="left">
+		This was a fantastic opportunity; Google invited teams of college students to compete over tiny challenges, some programming, some logic puzzles, some that required pop culture knowledge. It was a day of solving problems and working as a team to use each persons' strengths. Our team solved the most challenges and won the competition, but the real reward was just being in that room and having fun being computer science students.
+        </p>
       </div>
       <img src="./GoogleIrvine.jpeg"  alt="Google Irvine image" style="width:100%">
       
@@ -51,8 +86,10 @@
 
     <div class="polaroid">
      <div class="caption">
-        <p>Ragan Lab:
-        </br>I left INRF because I wanted to learn more about machine learning. I found a grad student that was working on his PhD in Material Science. William Thrift was an amazing mentor and introduction to machine learning. We had a goal of what we wanted for the paper he was working on, but we were both learning how to achieve that goal and I learned a lot of the basics from talking back and forth with him. The paper that I and a couple of other undergraduates helped him write ended being published in ACS Sensors. Which makes me a published author, this was a fun benefit but what I got from working in the Ragan Lab was tons of knowledge about machine learning that would help me learn more on my own. Below is the image from the abstract of that paper</p>
+        <p><p class='bolder'>Ragan Lab</p>
+	</p>
+	<p class="left">
+        I left INRF because I wanted to learn more about machine learning. I found a grad student that was working on his PhD in Material Science. William Thrift was an amazing mentor and introduction to machine learning. We had a goal of what we wanted for the paper he was working on, but we were both learning how to achieve that goal and I learned a lot of the basics from talking back and forth with him. The paper that I and a couple of other undergraduates helped him write was published in ACS Sensors. What I got from working in the Ragan Lab was tons of knowledge about machine learning that would help me learn more on my own. Below is an image from the abstract of that paper depicting our process. </p>
       </div>
       <img src="./RaganACS.gif"  alt="Picture from acs paper"  style="width:100%">
       
@@ -60,8 +97,10 @@
 
     <div class="polaroid">
      <div class="caption">
-        <p>INRF:
-        </br>During my first year at UCI I was incredibly fortunate to find a lab that needed fullstack work done and had the time to let me learn. I worked with one software developer and a lab full of engineers to provide meaningful information. Technicians would create circuits in this lab and the process of doing that is very inconsistent. To help them understand why their experiments were failing, I built a system that measured the climate in the lab at any particular moment. So they could look back and see if the lab spiked out of reasonable conditions during their experiment. One of the best parts of this project was the lack of direction, we had a final goal but other than that, I was given a budget and blank desktop computer. So I installed linux on the computer and went to work making a network of raspberry pi's to measure the climate and report it to the main server. From there I made the API public and a more senior engineer took that data and integrated it into the interface you see below. The second picture is the raspberry pi's before they were installed</p>
+        <p><p class='bolder'>INRF</p>
+	</p>
+	<p class="left">
+        During my first year at UCI I was incredibly fortunate to find a lab that needed fullstack work done and had the time to let me learn. I worked with one software developer and a lab full of engineers to provide meaningful information. Technicians would create circuits in this lab and the process of doing that is very inconsistent. To help them understand why their experiments were failing, I built a system that measured the climate in the lab at any particular moment. So they could look back and see if the lab spiked out of reasonable conditions during their experiment. One of the best parts of this project was the lack of direction; we had a final goal but other than that, I was given a budget and blank desktop computer. So, I installed Linux on the computer and went to work making a network of raspberry Pi's to measure the climate and report it to the main server. From there I made the API public and a more senior engineer took that data and integrated it into the interface you see below. The second picture is the raspberry Pi's before they were installed. </p>
       </div>
       <img src="./inrf_temp_humid.gif"  alt="gif showing inrf system" style="width:100%">
       <img src="./raspberryPi.jpg"  alt="gif showing inrf system" style="width:100%">
